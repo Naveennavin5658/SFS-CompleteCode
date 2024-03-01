834df6a64dbb08a210d4238cb5e42d9ca0d5637e
john4suresh <johnsuresh777@gmail.com>
2024-03-02 00:40:21 +0530

    Added new Header and tailwind config

--- src/Header.js
+++ src/Header.js
@@ -1,60 +1,61 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import "./Header.css"; // Import the CSS file
+// import "./Header.css";
 import logo from ".//assests/logo.png";
 import { LuInstagram } from "react-icons/lu";
 import { FaYoutube } from "react-icons/fa";
 import { CiFacebook } from "react-icons/ci";
 import sfsEdit1 from "./assests/sfsHome.jpg";
-import {FiMapPin, FiPhoneCall} from "react-icons/fi"
-import { TbDeviceLandlinePhone} from "react-icons/tb"
+import { FiMapPin, FiPhoneCall } from "react-icons/fi";
+import { TbDeviceLandlinePhone } from "react-icons/tb";
 import { SiGmail } from "react-icons/si";
 
 function Header() {
-  // const openLinkInNewTab = (url) => {
-  //   window.open(url, "_blank");
-  // };
   return (
-    <header className="header">
-      <div className="header-navbar-content">
-        <nav className="navbar">
-          <ul>
-            <li>
-              <NavLink exact to="/" activeClassName="active-link">
+    <header className="header relative">
+        <nav className="absolute right-0 top-6 flex items-center justify-between w-full px-4 pl-10">
+        <img
+        src={logo}
+        alt="School Logo"
+        className="h-24 w-24 rounded-full"
+      />
+          <ul className="ml-auto flex gap-5 w-fit">
+            <li className="list-item">
+              <NavLink exact to="/" activeClassName="border border-white" className="text-white hover:border hover:border-white py-3 px-1.5 cursor-pointer">
                 Home
               </NavLink>
             </li>
             <li>
-              <NavLink to="/about-us" activeClassName="active-link">
+              <NavLink to="/about-us" activeClassName="border border-white" className="text-white hover:border hover:border-white py-3 px-1.5 cursor-pointer">
                 About Us
               </NavLink>
             </li>
             <li>
-              <NavLink to="/gallery" activeClassName="active-link">
+              <NavLink to="/gallery" activeClassName="border border-white" className="text-white hover:border hover:border-white py-3 px-1.5 cursor-pointer">
                 Gallery
               </NavLink>
             </li>
             <li>
-              <NavLink to="/results" activeClassName="active-link">
+              <NavLink to="/results" activeClassName="border border-white" className="text-white hover:border hover:border-white py-3 px-1.5 cursor-pointer">
                 Results
               </NavLink>
             </li>
             <li>
-              <NavLink to="/alumni" activeClassName="active-link">
+              <NavLink to="/alumni" activeClassName="border border-white" className="text-white hover:border hover:border-white py-3 px-1.5 cursor-pointer">
                 Alumni
               </NavLink>
             </li>
             <li>
               <NavLink
                 to="/sfs-alumni-foundation"
-                activeClassName="active-link"
+                activeClassName="border border-white"
+                className="text-white hover:border hover:border-white py-3 px-1.5 cursor-pointer"
               >
                 Alumni Foundation
               </NavLink>
             </li>
           </ul>
         </nav>
-      </div>
       <img
         className="background-image"
         src={sfsEdit1}
@@ -64,44 +65,58 @@ function Header() {
         }}
         alt="sample"
       />
-      <img
-        className="header-logo"
-        src={logo}
-        alt="School Logo"
-        style={{ height: "90px", width: "90px", borderRadius: "100%" }}
-      />
 
-      <div className="address-container1">
-        <div className="address">
+      <div className="absolute bottom-0 flex flex-col md:flex-row items-start md:items-center justify-between w-full px-10 pb-5">
+        <div className="flex flex-col gap-5">
+        <div className="flex items-center text-white gap-5">
           <FiMapPin size={25} />
-        <p>
-          <b>NGO'S Colony, Palangi</b> <br/>
-          <b>Tanuku, Andhra Pradesh, 534211</b>
-        </p>
+          <p>
+            <b>NGO'S Colony, Palangi</b> <br />
+            <b>Tanuku, Andhra Pradesh, 534211</b>
+          </p>
         </div>
-        <div className="address">
-        <TbDeviceLandlinePhone size={25} />
-        <p>
-         <b>08819 - 224667</b>
-        </p>
+        <div className="flex items-center text-white gap-5">
+          <TbDeviceLandlinePhone size={25} />
+          <p>
+            <b>08819 - 224667</b>
+          </p>
         </div>
-        <div className="address">
+        <div className="flex items-center text-white gap-5">
           <FiPhoneCall size={25} />
-        <p>
-         <b>9440828636</b>
-        </p>
+          <p>
+            <b>9440828636</b>
+          </p>
         </div>
-        <div className="address">
+        <div className="flex items-center text-white gap-5">
           <SiGmail size={25} />
-        <p>
-          <b>sfstanuku.wg@gmail.com</b>
-        </p>
+          <p>
+            <b>sfstanuku.wg@gmail.com</b>
+          </p>
         </div>
-        <div className="header-right">
-          <a href="https://www.instagram.com/" rel="noreferrer" target={"_blank"}><LuInstagram size={40} color="white" /></a>
-          <a href="https://www.instagram.com/" rel="noreferrer" target={"_blank"}><CiFacebook size={40} color="white" /></a>
-          <a href="https://www.instagram.com/" rel="noreferrer" target={"_blank"}><FaYoutube size={40} color="white" /></a>
-          
+        </div>
+        
+        <div className="flex items-center gap-5">
+          <a
+            href="https://www.instagram.com/"
+            rel="noreferrer"
+            target={"_blank"}
+          >
+            <LuInstagram size={40} color="white" />
+          </a>
+          <a
+            href="https://www.instagram.com/"
+            rel="noreferrer"
+            target={"_blank"}
+          >
+            <CiFacebook size={40} color="white" />
+          </a>
+          <a
+            href="https://www.instagram.com/"
+            rel="noreferrer"
+            target={"_blank"}
+          >
+            <FaYoutube size={40} color="white" />
+          </a>
         </div>
       </div>
     </header>
