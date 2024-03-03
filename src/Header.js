587bdc18359d7f53549a9b8e350423a7ea396ce6
guptadhairy <21it3032@rgipt.ac.in>
2024-03-03 12:46:56 +0530

    Changes Done

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-// import "./Header.css";
+import "./Header.css";
 import logo from ".//assests/logo.png";
 import { LuInstagram } from "react-icons/lu";
 import { FaYoutube } from "react-icons/fa";
@@ -9,11 +9,13 @@ import sfsEdit1 from "./assests/sfsHome.jpg";
 import { FiMapPin, FiPhoneCall } from "react-icons/fi";
 import { TbDeviceLandlinePhone } from "react-icons/tb";
 import { SiGmail } from "react-icons/si";
+import DrawerExample from "./DrawerExample";
+
 
 function Header() {
   return (
     <header className="header relative">
-        <nav className="absolute right-0 top-6 flex items-center justify-between w-full px-4 pl-10">
+        <nav className="absolute right-0 top-6 flex items-center justify-between w-full px-4 pl-10 nav1">
         <img
         src={logo}
         alt="School Logo"
@@ -21,12 +23,12 @@ function Header() {
       />
           <ul className="ml-auto flex gap-5 w-fit">
             <li className="list-item">
-              <NavLink exact to="/" activeClassName="border border-white" className="text-white hover:border hover:border-white py-3 px-1.5 cursor-pointer">
+              <NavLink  to="/" activeClassName="border border-white" className="text-white hover:border hover:border-white py-3 px-1.5 cursor-pointer">
                 Home
               </NavLink>
             </li>
             <li>
-              <NavLink to="/about-us" activeClassName="border border-white" className="text-white hover:border hover:border-white py-3 px-1.5 cursor-pointer">
+              <NavLink  to="/about-us" activeClassName="border border-white" className="text-white hover:border hover:border-white py-3 px-1.5 cursor-pointer">
                 About Us
               </NavLink>
             </li>
@@ -59,14 +61,9 @@ function Header() {
       <img
         className="background-image"
         src={sfsEdit1}
-        style={{
-          height: "520px",
-          width: "100%",
-        }}
         alt="sample"
       />
-
-      <div className="absolute bottom-0 flex flex-col md:flex-row items-start md:items-center justify-between w-full px-10 pb-5">
+      <div className="absolute bottom-0 flex flex-col md:flex-row items-start md:items-center justify-between w-full px-10 pb-5 nav-m">
         <div className="flex flex-col gap-5">
         <div className="flex items-center text-white gap-5">
           <FiMapPin size={25} />
@@ -119,6 +116,7 @@ function Header() {
           </a>
         </div>
       </div>
+      <DrawerExample/>
     </header>
   );
 }
